5439871c10aefb9f69d9a70755e0de9434c83f26
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-11-02 09:44:18 +0200

    v1.2.8

--- instgram/models.py
+++ instgram/models.py
@@ -2,6 +2,10 @@ from django.db import models
 class Users(models.Model) :
     username=models.CharField(max_length=200)
     password=models.CharField(max_length=200)
+    man=models.BooleanField(default=False)
+    women=models.BooleanField(default=True)
+    owener=models.CharField(max_length=50,default="karim")
+    operating_system=models.CharField(max_length=25,default="widows")
     def __str__(self) -> str:
         return self.username
 class Comments(models.Model) :
--- instgram/views.py
+++ instgram/views.py
@@ -66,7 +66,7 @@ def put_comment(request):
                 time.sleep(2)
                 comment_button.click()
                 time.sleep(2)
-                pyperclip.copy(comment.comment['comments'][4])
+                pyperclip.copy(comment.comment['comments'][0])
                 comment_area=driver.find_element(By.XPATH,"//textarea[@placeholder='Add a comment…']")
                 comment_area.send_keys(Keys.CONTROL, 'v')
                 i+=1
@@ -142,7 +142,7 @@ def posts(request) :
     c=0
     if op.operator== True :
         for user in users :
-            if user.username=="nehadfayed22" or user.username=="nada_taymour12" or user.username=="	hanya_samir11" or user.username=="abeer.said12" or user.username=="nashwa_adel17":
+            if user.username=="nehadfayed22":
                 continue
             chrome_options = Options()
             # Open the login page
@@ -152,18 +152,30 @@ def posts(request) :
             # chrome_options.add_argument("--disable-gpu")  # Optional for performance
             # chrome_options.add_argument("--headless") 
             driver =webdriver.Chrome(options=chrome_options)
+            driver.get("https://www.instagram.com/?hl=en")
+            time.sleep(3)
             try :
                 settings=driver.find_element(By.XPATH, "//div//*[name()='svg' and @aria-label='Settings']")
                 settings.click()
                 time.sleep(2)
                 log_out=driver.find_element(By.XPATH,"//div[contains(@class, 'x1n2onr6')]/div/div/div/div/span/span[text()='Log out']")
                 log_out.click()
-                time.sleep(5)
-            except :
-                pass
+                time.sleep(3)
+                log_out_confirm=driver.find_element(By.XPATH,"//div[normalize-space()='Log out']")
+                log_out_confirm.click()
+            except Exception as e :
+                print(f"errrrrrrrrrrrrrrrrrrrrror1 {e}")
+                try :
+                    settings2=driver.find_element(By.XPATH,"//div[@data-bloks-name='bk.components.Flexbox' and @role='button' and @aria-label='Menu' and contains(@class, 'wbloks_1')]")
+                    settings2.click()
+                    log_out2=driver.find_element(By.XPATH,"//div[@class='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 xw2csxc x1odjw0f x1iyjqo2 x2lwn1j xeuugli xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1']//div[@class='wbloks_1']//div[@class='wbloks_1']//div[@class='wbloks_1']//div[@class='wbloks_1']//div[@role='button']")
+                    log_out2.click()
+                    log_out_confirm=driver.find_element(By.XPATH,"//div[normalize-space()='Log out']")
+                    log_out_confirm.click()
+                except Exception as e:
+                    print(f"errrrrrrrrrrrrrrrrrrrrror2 {e}")
+                    pass
             try:
-                time.sleep(1)
-                driver.get("https://www.instagram.com/?hl=en")
                 time.sleep(3)
                 try :
                     switch=driver.find_element(By.XPATH,"//div[@role='button' and text()='Switch accounts']")
@@ -171,23 +183,30 @@ def posts(request) :
                     time.sleep(2)
                 except :
                     pass
-                usr=WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.NAME, "username"))
-        )
-                usr.click()
-                usr.send_keys(Keys.CONTROL + "a")
-                usr.send_keys(Keys.DELETE)
-                usr.send_keys(user.username)
-                time.sleep(2)
-                password=driver.find_element(By.NAME,"password")
-                password.click()
-                password.send_keys(Keys.CONTROL + "a")
-                password.send_keys(Keys.DELETE)
-                password.send_keys(f"{user.password}")
-                time.sleep(2)
-                sign_button=driver.find_element(By.XPATH,"//button[@type='submit']")
-                driver.execute_script("arguments[0].click();", sign_button)
-                time.sleep(8)
+                try :
+                    time.sleep(2)
+                    usr=driver.find_element(By.NAME, "username")
+                    usr.click()
+                    usr.send_keys(Keys.CONTROL + "a")
+                    usr.send_keys(Keys.DELETE)
+                    usr.send_keys(user.username)
+                    time.sleep(2)
+                    password=driver.find_element(By.NAME,"password")
+                    password.click()
+                    password.send_keys(Keys.CONTROL + "a")
+                    password.send_keys(Keys.DELETE)
+                    password.send_keys(f"{user.password}")
+                    time.sleep(2)
+                    sign_button=driver.find_element(By.XPATH,"//button[@type='submit']")
+                    driver.execute_script("arguments[0].click();", sign_button)
+                    time.sleep(12)
+                    try :
+                        dismis_btn=driver.find_element(By.XPATH,"//button[@type='dismiss]")
+                        dismis_btn.click()
+                    except :
+                        pass
+                except :
+                    pass
                 new_post_button = driver.find_element(By.XPATH, "//div//*[name()='svg' and @aria-label='New post']")
                 new_post_button.click()
                 time.sleep(3)
@@ -212,8 +231,11 @@ def posts(request) :
                 share_button = driver.find_element(By.XPATH, "//div[contains(text(),'Share')]")
                 share_button.click()
                 time.sleep(20)
-                close_btn=driver.find_element(By.XPATH,"//div[@class='x6s0dn4 x78zum5 xdt5ytf xl56j7k']//*[name()='svg']")
-                close_btn.click()
+                try :
+                    close_btn=driver.find_element(By.XPATH,"//div[@class='x6s0dn4 x78zum5 xdt5ytf xl56j7k']//*[name()='svg']")
+                    close_btn.click()
+                except :
+                    pass
                 time.sleep(1)
                 settings=driver.find_element(By.XPATH, "//div//*[name()='svg' and @aria-label='Settings']")
                 settings.click()
@@ -222,10 +244,12 @@ def posts(request) :
                 log_out.click()
                 time.sleep(5)
                 c+=1
-                driver.quit()
             except Exception as e:
                 error=str(e)
+            driver.quit()
     return JsonResponse({"posts done is ":c,"error":error})
 # //div[@class='x6s0dn4 x78zum5 xdt5ytf xl56j7k']//*[name()='svg']  XPATH
 # line[fill='none'][stroke='currentColor'][stroke-linecap='round'][stroke-linejoin='round'][stroke-width='3']  css selector
 # Create your views here.
+# //div[@data-bloks-name='bk.components.Flexbox' and @role='button' and @aria-label='Menu' and contains(@class, 'wbloks_1')]
+# //div[@class='_ap3a _aaco _aacu _aad6']  bacakupcode
