308595ead0842dac521cfe280153818666d52228
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-11-11 22:46:40 +0200

    dsbsx

--- instgram/admin.py
+++ instgram/admin.py
@@ -3,4 +3,7 @@ from .models import *
 admin.site.register(Users)
 admin.site.register(Comments)
 admin.site.register(Operator)
+admin.site.register(Post)
+admin.site.register(Gmail)
+admin.site.register(FacebookAccount)
 # Register your models here.
