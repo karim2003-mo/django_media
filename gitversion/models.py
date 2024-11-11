b7d963576b057045bd89cdf3b2ad253d5e55ebeb
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-11-11 23:30:02 +0200

    special commit

---
+++ gitversion/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+from instgram.models import *
+# Create your models here.
---
+++ gitversion/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
+def test(request) :
+    users = Users.objects.all()
+    l=[]
+    for user in users :
+        l.append(user)
+    return JsonResponse({"result":l})
+# Create your views here.
